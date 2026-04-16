d8efe9f27147ee3d04b2d792a03df170819bd0e2
Shonhh <endinja.versitile@gmail.com>
2026-04-16 08:09:53 -0500

    fixed neovim eval warning

--- modules/apps/tui/neovim.nix
+++ modules/apps/tui/neovim.nix
@@ -15,7 +15,11 @@
       options.mySystem.apps.neovim.enable = lib.mkEnableOption "Neovim Editor";
 
       config = lib.mkIf cfg.enable {
-        programs.neovim.enable = true;
+        programs.neovim = {
+          enable = true;
+          programs.neovim.withRuby = false;
+          programs.neovim.withPython3 = false;
+        };
 
         home-manager.users.shonh.programs.neovim = {
           enable = true;
